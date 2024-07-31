39b0f7279fd9d0552d42f0a2c714414264fdcb93
vikgi <vikasgv02@gmail.com>
2024-08-01 00:14:20 +0530

    bug fixes

--- src/components/workspace/Workspace.tsx
+++ src/components/workspace/Workspace.tsx
@@ -3,7 +3,7 @@ import Split from 'react-split'
 import ProblemDescription from './ProblemDescription/ProblemDescription';
 import Playground from './Playground/Playground';
 import { Problem } from '@/utils/types/problem';
-import Confetti from 'react-Confetti';
+import Confetti from 'react-confetti';
 import useWindowSize from '@/hooks/useWindowSize';
 
 
